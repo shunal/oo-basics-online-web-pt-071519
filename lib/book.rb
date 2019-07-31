3baaf31e48b29857b75f6b84f1057ce9487a7fa6
Steven Hunal <stevehunal@gmail.com>
2019-07-31 17:38:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,4 +8,27 @@ class Book
     @title
   end
   
+  def author=(author)
+    @author = author
+  end 
+  
+  def author 
+    @author 
+  end 
+  
+  def page_count(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre 
+  end 
+  
+  def genre 
+    @genre
+  end
 end 
